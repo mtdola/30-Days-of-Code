ed0856f418508c759ce83c8c87a8480bd4b91d11
mtdola <mtdolaj@gmail.com>
2022-04-05 09:53:30 +0100

    first push

---
+++ Javascript Course/demo/src/Application.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <span> This is my first react component. It's interesting</span>
+            </div>
+        );
+    }
+
+}
+
+export default Application;
